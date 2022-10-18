e876a6af73c2efd53e6b3b5e9cd884117f80fb3a
Tomasz Kielkowski <71585480+t-kielkowski@users.noreply.github.com>
2022-10-18 21:26:23 +0200

    GetSensorDataQuery and GetSensorDataQueryHandler

---
+++ WeightCharts.Application/Feature/Beehive/GetSensorDataQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace WeightCharts.Application.Feature.Beehive
+{
+    public class GetSensorDataQuery : IRequest<Unit>
+    {
+        public int Id { get; set; }
+
+        public GetSensorDataQuery(int id) => Id = id;
+    }
+}
---
+++ WeightCharts.Application/Feature/Beehive/GetSensorDataQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace WeightCharts.Application.Feature.Beehive
+{
+    public class GetSensorDataQueryHandler : IRequestHandler<GetSensorDataQuery, Unit>
+    {
+        public GetSensorDataQueryHandler()
+        {
+        }
+
+        public Task<Unit> Handle(GetSensorDataQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
