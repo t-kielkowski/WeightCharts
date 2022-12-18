f3ce026201e7109c895687bdfa73f65b2ae4499b
Tomasz Kielkowski <71585480+t-kielkowski@users.noreply.github.com>
2022-12-18 21:04:01 +0100

    ApiConfiguration

---
+++ WeightCharts.Application/Options/ApiConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace WeightCharts
+{
+    public class ApiConfiguration
+    {
+        public string BaseApiUrl { get; set; }
+        public string BeehiveListUrl { get; set; }
+        public string TemperatureUrl { get; set; }
+        public string MoistureUrl { get; set; }
+        public string WeightUrl { get; set; }
+    }
+}
