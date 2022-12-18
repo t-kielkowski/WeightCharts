e7e13fdf7288050d2182401d0fa5cea4006f1c19
Tomasz Kielkowski <71585480+t-kielkowski@users.noreply.github.com>
2022-12-18 21:06:20 +0100

    moving functions to separate files

---
+++ WeightChart/wwwroot/js/sidebar.js
@@ -0,0 +1,41 @@
+﻿$(function ()
+{
+    $('#sidebar-toggle-button').on('click', function () {
+        $('#sidebar').toggleClass('sidebar-toggle');
+        $('#page-content-wrapper').toggleClass('page-content-toggle');
+        fireResize();
+        $('#gridContainer').toggleClass('widt-100vw')
+    });
+
+    $('#sidebar').on('show.bs.collapse', function () {
+        $('#sidebar').find('.collapse.in').collapse('hide');
+    });
+
+    (function addActiveClassToNestedList() {
+        var pageURL = $(location).attr('href');
+        var URLSplits = pageURL.split('/');
+
+        var routeURL = '/' + URLSplits[URLSplits.length - 3] + '/' + URLSplits[URLSplits.length - 2] + '/';
+        var activeNestedList = $('.sub-menu > li > a[href="' + routeURL + '"]').parent();
+
+        if (activeNestedList.length !== 0 && !activeNestedList.hasClass('active')) {
+            $('.sub-menu > li').removeClass('active');
+            activeNestedList.addClass('active');
+        }
+    })();
+
+    function fireResize()
+    {
+        if (document.createEvent) {
+            var ev = document.createEvent('Event');
+            ev.initEvent('resize', true, true);
+            window.dispatchEvent(ev);
+        }
+        else {
+            element = document.documentElement;
+            var event = document.createEventObject();
+            element.fireEvent("onresize", event);
+        }
+    }
+
+});
