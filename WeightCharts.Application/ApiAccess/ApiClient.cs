e2fcce6997dd8d4afac9c38c70e9c3094abe9d3e
Tomasz Kielkowski <71585480+t-kielkowski@users.noreply.github.com>
2022-12-18 21:01:29 +0100

    added ApiClient

---
+++ WeightCharts.Application/ApiAccess/ApiClient.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Extensions.Options;
+using RestSharp;
+
+namespace WeightCharts.Application.ApiAccess
+{
+    public class ApiClient : IApiClient
+    {
+        private readonly ApiConfiguration _apiConfiguration; 
+        private RestClient _client;
+
+        public ApiClient(IOptions<ApiConfiguration> apiConfiguration)
+        {
+            _apiConfiguration = apiConfiguration.Value;
+            _client = new RestClient(apiConfiguration.Value.BaseApiUrl);
+        }
+
+        public async Task<RestResponse> GetBeehiveList()
+        {            
+            var getRequest = new RestRequest(_apiConfiguration.BeehiveListUrl, Method.Get);
+            var response = await _client.ExecuteGetAsync(getRequest);
+
+            return response;
+        }
+
+        public async Task<RestResponse> GetTemperatureData(IWeightReadingsSearchParams searchParams)
+        {
+            var getRequest = new RestRequest(_apiConfiguration.TemperatureUrl, Method.Get);
+            getRequest = AddParameters(getRequest, searchParams);
+            var response = await _client.ExecuteGetAsync(getRequest);
+
+            return response;
+        }
+
+        public async Task<RestResponse> GetMoistureData(IWeightReadingsSearchParams searchParams)
+        {
+            var getRequest = new RestRequest(_apiConfiguration.MoistureUrl, Method.Get);
+            getRequest = AddParameters(getRequest, searchParams);
+            var response = await _client.ExecuteGetAsync(getRequest);
+
+            return response;
+        }
+
+        public async Task<RestResponse> GetWeightData(IWeightReadingsSearchParams searchParams)
+        {
+            var getRequest = new RestRequest(_apiConfiguration.WeightUrl, Method.Get);
+            getRequest = AddParameters(getRequest, searchParams);
+            var response = await _client.ExecuteGetAsync(getRequest);
+
+            return response;
+        }
+
+        private RestRequest AddParameters(RestRequest request, IWeightReadingsSearchParams searchParams)
+        {
+            request.AddParameter("id", searchParams.WeightId);
+            request.AddParameter("dateFrom", searchParams.DateFrom?.ToString());
+            request.AddParameter("dateTo", searchParams.DateTo?.ToString());
+
+            return request;
+        }
+    }
+}
---
+++ WeightCharts.Application/ApiAccess/IApiClient.cs
@@ -0,0 +1,12 @@
+﻿using RestSharp;
+
+namespace WeightCharts.Application.ApiAccess
+{
+    public interface IApiClient
+    {
+        Task<RestResponse> GetBeehiveList();
+        Task<RestResponse> GetTemperatureData(IWeightReadingsSearchParams searchParams);
+        Task<RestResponse> GetMoistureData(IWeightReadingsSearchParams searchParams);
+        Task<RestResponse> GetWeightData(IWeightReadingsSearchParams searchParams);
+    }
+}
