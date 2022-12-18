0e4b7408873c01b58734caf1333f0d828b96e371
Tomasz Kielkowski <71585480+t-kielkowski@users.noreply.github.com>
2022-12-18 21:05:28 +0100

    Added services to the container

--- WeightChart/Program.cs
+++ WeightChart/Program.cs
@@ -1,14 +1,19 @@
 using MediatR;
+using WeightCharts;
+using WeightCharts.Application.ApiAccess;
 using WeightCharts.Application.Feature.Beehive;
+using WeightCharts.Application.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews(); 
-builder.Services.AddMediatR(typeof(GetSensorDataQuery));
+builder.Services.AddControllersWithViews();
+builder.Services.Configure<ApiConfiguration>(builder.Configuration.GetSection("ApiConfiguration"));
+builder.Services.AddTransient<IApiClient, ApiClient>();
+builder.Services.AddTransient<IBeehiveNameList, BeehiveNameList>();
+builder.Services.AddMediatR(typeof(GetSensorTempDataQuery));
 
 var app = builder.Build();
-
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
